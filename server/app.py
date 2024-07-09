0b5933b2323dd2a9b42033e66f71ad81de538a63
kbtanvir <tanvirkhaan004@gmail.com>
2024-07-09 13:17:12 +0600

    delete actions added on form

--- server/app.py
+++ server/app.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask, request, render_template
+import shutil
+from flask import Flask, request, render_template, redirect, url_for
 from werkzeug.utils import secure_filename
 from models import db, User
 from dotenv import load_dotenv
 from flask_migrate import Migrate
+
 
 load_dotenv()
 
@@ -53,6 +55,30 @@ def upload_file():
 
     users = User.query.all()
     return render_template('index.html', users=users)
+
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all_users():
+    users = User.query.all()
+    for user in users:
+        subdomain_path = os.path.join(app.config['UPLOAD_FOLDER'], user.subdomain)
+        if os.path.exists(subdomain_path):
+            shutil.rmtree(subdomain_path)
+    User.query.delete()
+    db.session.commit()
+    return redirect(url_for('upload_file'))
+
+
+@app.route('/delete/<int:user_id>', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        subdomain_path = os.path.join(app.config['UPLOAD_FOLDER'], user.subdomain)
+        if os.path.exists(subdomain_path):
+            shutil.rmtree(subdomain_path)
+        db.session.delete(user)
+        db.session.commit()
+    return redirect(url_for('upload_file'))
 
 if __name__ == '__main__':
     with app.app_context():
